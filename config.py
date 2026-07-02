8e6cdb4307fa667e04882bd84330399cf8cd0b86
khatunsarbina403-source <khatunsarbina403@gmail.com>
2026-07-03 00:16:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN = os.getenv("8989139227:AAEFULzmAgPYNOLtKMnaKek2W2-Pr1LzXA8")
-CHAT_ID = os.getenv("7947347734")
+BOT_TOKEN = os.getenv("8929716589:AAFTV7R4MO-fQehRrqdz5mAJlnuBxqutlsg")
+CHAT_ID = os.getenv("7852006891")
